ba22e0d5a8eeb9d901dafb959734f8da69063396
laterkhaoula <laterkhaoula@gmail.com>
2026-07-24 12:14:59 +0100

    corection de structure de code

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,7 @@ use App\Models\Employe;
 use App\Models\Entreprise;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    if (auth()->check()) {
-        return redirect()->route('dashboard');
-    }
-    return view('welcome');
-});
+git add .
 
 Route::get('/dashboard', function () {
     $user = auth()->user();
